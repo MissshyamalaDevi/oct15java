d2768e82fbc67b9aa9988cf1d17c1c8579c9054d
MissshyamalaDevi <baskaranshyamaladevi@gmail.com>
2022-10-15 16:22:36 +0530

    saturday new javaproject oct15

---
+++ src/org/sample/Practise.java
@@ -0,0 +1,28 @@
+package org.sample;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Practise {
+	
+	public static void main (String[] args) throws AWTException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ACER\\eclipse-workspace\\SeleniumProject\\drivers\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://demo.automationtesting.in/Alerts.html");
+		
+		
+		
+		
+
+}
+
+}
